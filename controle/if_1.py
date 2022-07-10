9182b06f1f6a5c7f5637300be5046a53abcea83b
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-10 19:19:20 -0300

    if

--- controle/if_1.py
+++ controle/if_1.py
@@ -1,6 +1,7 @@
 nota = float(input('Informe a nota do aluno: '))
+comportado = True if input('Comportado (s/n): ') == 'y' else False
 
-if nota >= 9:
+if nota >= 9 and comportado:
     print('Duas palavras: para bens!')
     print('Quadro de honra')
 elif nota >= 7:
