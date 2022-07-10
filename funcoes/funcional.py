9200ef9495648ff09a97704af46ffc8263a27d93
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-10 20:43:58 -0300

    funcional

---
+++ funcoes/funcional.py
@@ -0,0 +1,32 @@
+def soma(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+somar = soma
+print(somar(3, 4))
+
+
+def operacao_aritmetica(fn, op1, op2):
+    return fn(op1, op2)
+
+
+resultado_soma = operacao_aritmetica(soma, 13, 48)
+print(resultado_soma)
+
+resultado_sub = operacao_aritmetica(sub, 13, 48)
+print(resultado_sub)
+
+
+def soma_parcial(a):
+    def concluir_soma(b):
+        return a + b
+
+    return concluir_soma
+
+
+resultado_final = soma_parcial(10)(12)
+print(resultado_final)
